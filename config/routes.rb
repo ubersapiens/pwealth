b4253745140e2c61d371cc6d0b0d07d60a34f6a1
Juan Pardini <jfpardini@gmail.com>
2017-10-10 15:53:22 +0100

    Precompile.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,30 @@ Rails.application.routes.draw do
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
   get 'accounting_office_secretarial_services' => 'main#accounting_office_secretarial_services'
-  get 'legal' => 'main#legal'
+  get 'corporate_multi_family_office' => 'main#corporate_multi_family_office'
+  get 'international_tax_planning' => 'main#international_tax_planning'
+  get 'succession_planning' => 'main#succession_planning'
+  get 'wealth_asset_management' => 'main#wealth_asset_management'
+  get 'redomiciliation_companies_foundations' => 'main#redomiciliation_companies_foundations'
+  get 'incorporation_ibcs' => 'main#incorporation_ibcs'
+  get 'incorporation_foundations' => 'main#incorporation_foundations'
+  get 'corporate_international_tax_planning' => 'main#corporate_international_tax_planning'
+  get 'reinsurance_services' => 'main#reinsurance_services'
+  get 'banking_services' => 'main#banking_services'
+  get 'family_offices' => 'main#family_offices'
+  get 'immigration/citizenship_antigua_barbuda' => 'main#citizenship_antigua_barbuda'
+  get 'immigration/citizenship_cyprus' => 'main#citizenship_cyprus'
+  get 'immigration/citizenship_dominica' => 'main#citizenship_dominica'
+  get 'immigration/citizenship_grenada' => 'main#citizenship_grenada'
+  get 'immigration/citizenship_malta' => 'main#citizenship_malta'
+  get 'immigration/citizenship_st_kitts_nevis' => 'main#citizenship_st_kitts_nevis'
+  get 'immigration/citizenship_st_lucia' => 'main#citizenship_st_lucia'
+  get 'immigration/residency_cyprus' => 'main#residency_cyprus'
+  get 'immigration/residency_malta' => 'main#residency_malta'
+  get 'immigration/residency_monaco' => 'main#residency_monaco'
+  get 'immigration/residency_panama' => 'main#residency_panama'
+  get 'immigration/second_passport_panama' => 'main#second_passport_panama'
+  get 'real_estate' => 'main#real_estate'
+
 
 end
