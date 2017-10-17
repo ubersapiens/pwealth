5f713955ad30dc010829fce8c4f16a28dd2b4c32
Juan Pardini <jfpardini@gmail.com>
2017-10-17 11:42:09 +0200

    Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,44 @@
 CmsPage.delete_all
 
 #Wealth
-CmsPage.create({"title"=>"Accounting", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Multi Family Office", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"International Tax Planning", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Succession Planning", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Wealth Management", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+
 
 #Immigration
-CmsPage.create({"title"=>"Accounting", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Antigua and Barbuda Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Cyprus Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Dominica Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Grenada Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Malta Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"St Kitts and Nevis Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"St Lucia Citizenship", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+
+CmsPage.create({"title"=>"Cyprus Residency", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Malta Residency", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Monaco Residency", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Panama Residency", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+
+CmsPage.create({"title"=>"Panama Second Passport", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+
+CmsPage.create({"title"=>"Real Estate", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+
+CmsPage.create({"title"=>"Tax Residence", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+
+
 
 #Corporate Services
-CmsPage.create({"title"=>"Accounting", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Incorporation of Companies", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Incorporation of Foundations", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Formation of Trusts", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Reinsurance Services", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Banking Services", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Family Offices", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
 
 #Accounting
-CmsPage.create({"title"=>"Accounting", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
+CmsPage.create({"title"=>"Accounting and Secretarial Services", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
 
 #Legal
 CmsPage.create({"title"=>"Legal", "text_1"=>"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. "})
