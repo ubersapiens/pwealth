db72f29a3f8dbf2f4ac8361d496bf9c112f706ee
Juan Pardini <jfpardini@gmail.com>
2017-10-18 08:47:46 +0200

    Routes and links.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,83 +7,107 @@ class MainController < ApplicationController
   end
 
   #Pages
+  def multi_family_office
+    @cms_page = CmsPage.find(1)
+  end
+
+  def international_tax_planning
+    @cms_page = CmsPage.find(2)
+  end
+
+  def succession_planning
+    @cms_page = CmsPage.find(3)
+  end
+
+  def wealth_management
+    @cms_page = CmsPage.find(4)
+  end
+
+  def antigua_barbuda_citizenship
+    @cms_page = CmsPage.find(5)
+  end
+
+  def cyprus_citizenship
+    @cms_page = CmsPage.find(6)
+  end
+
+  def dominica_citizenship
+    @cms_page = CmsPage.find(7)
+  end
+
+  def grenada_citizenship
+    @cms_page = CmsPage.find(8)
+  end
+
+  def malta_citizenship
+    @cms_page = CmsPage.find(9)
+  end
+
+  def st_kitts_and_nevis_citizenship
+    @cms_page = CmsPage.find(10)
+  end
+
+  def st_lucia_citizenship
+    @cms_page = CmsPage.find(11)
+  end
+
+  def cyprus_residency
+    @cms_page = CmsPage.find(12)
+  end
+
+  def malta_residency
+    @cms_page = CmsPage.find(13)
+  end
+
+  def monaco_residency
+    @cms_page = CmsPage.find(14)
+  end
+
+  def panama_residency
+    @cms_page = CmsPage.find(15)
+  end
+
+  def panama_second_passport
+    @cms_page = CmsPage.find(16)
+  end
+
+  def real_estate
+    @cms_page = CmsPage.find(17)
+  end
+
+  def tax_residence
+    @cms_page = CmsPage.find(18)
+  end
+
+  def incorporation_of_companies
+    @cms_page = CmsPage.find(19)
+  end
+
+  def incorporation_of_foundations
+    @cms_page = CmsPage.find(20)
+  end
+
+  def formation_of_trusts
+    @cms_page = CmsPage.find(21)
+  end
+
+  def reinsurance_services
+    @cms_page = CmsPage.find(22)
+  end
+
+  def banking_services
+    @cms_page = CmsPage.find(23)
+  end
+
+  def family_offices
+    @cms_page = CmsPage.find(24)
+  end
+
   def accounting_office_and_secretarial_services
     @cms_page = CmsPage.find(25)
   end
 
   def legal
     @cms_page = CmsPage.find(26)
-  end
-
-  def corporate_multi_family_office
-  end
-
-  def international_tax_planning
-  end
-
-  def succession_planning
-  end
-
-  def wealth_asset_management
-  end
-
-  def redomiciliation_companies_foundations
-  end
-
-  def incorporation_ibcs
-  end
-
-  def incorporation_foundations
-  end
-
-  def corporate_international_tax_planning
-  end
-
-  def reinsurance_services
-  end
-
-  def banking_services
-  end
-
-  def family_offices
-  end
-
-  def citizenship_antigua_barbuda
-  end
-
-  def citizenship_cyprus
-  end
-
-  def citizenship_dominica
-  end
-
-  def citizenship_grenada
-  end
-
-  def citizenship_malta
-  end
-
-  def citizenship_st_kitts_nevis
-  end
-
-  def citizenship_st_lucia
-  end
-
-  def residency_cyprus
-  end
-
-  def residency_malta
-  end
-
-  def residency_monaco
-  end
-
-  def residency_panama
-  end
-
-  def second_passport_panama
-  end
-
-  def real_estate
   end
 end
